36b23bafef6fe34bea593458fe3dd8e17ef6554e
timelyportfolio <kent.russell@timelyportfolio.com>
2014-08-29 16:22:13 -0500

    barely functioning world cartogram
    based on http://prag.ma/code/d3-cartogram/#intlmig/2011 and
    http://th-mayer.de/cartogram/#nrlangs

--- code.R
+++ code.R
@@ -9,7 +9,27 @@ carto$LIB$name = "carto"
 
 
 carto$set(
-  map = "./world-50m.json"  #url to the topojson source
+  map = "worldcountries.topojson"  #url to the topojson source
 )
 
-carto
+carto$setTemplate(
+  chartDiv = "
+    <{{container}} id = '{{ chartId }}' class = '{{ lib }}' style='height:100%;width:100%;'>
+      <h1>Cartogram of the world's languages</h1>
+      <form>
+        <p>
+          <label>Scale by <select id='field'></select></label>
+          <span id='status'></span><br>
+        </p>
+      </form>
+      <div id='map-container'>
+        <!--img id='placeholder' alt='placeholder image for old browsers' src='placeholder.png'-->
+        <svg id = 'map' style = 'height:100%; width:100%' viewbox = '0 0 {{params.width}} {{params.height}}'>
+      </svg>
+      </div>
+    </{{ container}}>" 
+  , script = "./layouts/chart_mayer.html"
+)
+
+
+carto$save("world_carto.html",cdn=T)
